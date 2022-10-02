beed84776261eef83e4ec6b7285f920117f9ba5d
Ayaz <ayaz.panahov@aih.az>
2022-10-02 20:53:50 +0400

    Added some tests and business logic

--- app/src/main/java/lab4/App.java
+++ app/src/main/java/lab4/App.java
@@ -3,12 +3,53 @@
  */
 package lab4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
+
+    int someValue = 1;
+    List<Integer> someList = new ArrayList<Integer>();
+
     public String getGreeting() {
         return "Hello World!";
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+    public int getSomeValue() {
+        return someValue;
+    }
+
+    public long incementSomeValue(int value) {
+        someValue += value;
+        return someValue;
+    }
+
+    public long decrementSomeValue(int value) {
+        someValue -= value;
+        return someValue;
+    }
+
+    public float makeBOOM(){
+        return someValue/0;
+    }
+
+    public long addTwoIntegers(int a, int b) {
+        return a + b;
+    }
+
+    public double addTwoFloats(float a, float b) {
+        return a + b;
+    }
+
+    public long substractTwoIntegers(int a, int b) {
+        return a - b;
+    }
+
+    public double substractTwoFloats(float a, float b) {
+        return a - b;
+    }
+
+    public boolean appendToArray(int val){
+        return someList.add(val);
     }
 }
